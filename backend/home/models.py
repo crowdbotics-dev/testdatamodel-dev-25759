51da44b4c9a8349004f4e5af85985103f09f626a
Crowdbotics <engineering@crowdbotics.com>
2022-07-13 07:52:34 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class Test(models.Model):
     "Generated Model"
     test = models.BigIntegerField()
+    dwd = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
